b5e04214ed710f2421c15e0b80b03aebc723bc0c
Jorge Saldana <jojonoob134@gmail.com>
2023-09-07 14:43:51 -0500

    made the coversion

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool inInCode = false;
+            while (!inInCode)
+            {
+                Console.WriteLine("press 1 if you want to covert fahrenheit to celsius, press 2 if you want to covert celsius to fahrenheit, press 3 to close the code");
+                int chose = int.Parse(Console.ReadLine());
+                if (chose == 1)
+                {
+                    Console.WriteLine("what is the fahrenheit");
+                    int Temp = int.Parse(Console.ReadLine());
+                    TempConverter.FahrenheitToCelsius(Temp);
+                    Console.WriteLine("");
+                }
+                if (chose == 2)
+                {
+                    Console.WriteLine("what is the celsius");
+                    int Temp = int.Parse(Console.ReadLine());
+                    TempConverter.FahrenheitToCelsius(Temp);
+                    Console.WriteLine("");
+                }
+                if (chose == 3)
+                {
+                    inInCode = true;
+                }
+            }
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempConverter
+    {
+        public static void FahrenheitToCelsius(double Fahrenheit)
+        {
+            double Celsius = Fahrenheit - 32;
+            Celsius = (Celsius * 5) / 9;
+            Console.WriteLine($"It is {Celsius} celsius outside");
+        }
+        public static void CelsiusToFahrenheit(double Celsius)
+        {
+            double Fahrenheit = (Celsius * 9) / 5;
+            Fahrenheit += 32;
+            Console.WriteLine($"It is {Fahrenheit} fahrenheit outside");
+        }
+    }
+}
